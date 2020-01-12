05f71fad38a85ee93b0a62ce5b14359560f4b0d4
hidehidehorie <b1a1s2k3e5t8hupro@gmail.com>
2020-01-13 03:45:50 +0900

    Integrated initial drag and drop implementation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,13 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("html.sortable.js")
 
+require("jquery")
+require("jquery-ui")
 
+$(function(){
+  $('.sortable').sortable();
+});
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.

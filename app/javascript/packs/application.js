922f3750abfd9f2c044666940f419c8857a406cb
hidehidehorie <b1a1s2k3e5t8hupro@gmail.com>
2020-01-13 12:24:19 +0900

    Implemented persistent position updates for drag and drop interface

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,9 +11,7 @@ require("channels")
 require("jquery")
 require("jquery-ui")
 
-$(function(){
-  $('.sortable').sortable();
-});
+require("packs/portfolioSortable")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.

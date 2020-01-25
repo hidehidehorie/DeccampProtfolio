0f93cb0b8b7f5df7478f05d832941b63cf761bb0
hidehidehorie <b1a1s2k3e5t8hupro@gmail.com>
2020-01-25 19:02:52 +0900

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_cable.allowed_request_origins = ['https://hidehide-portfolio.herokuapp.com/', 'http://hidehide-portfolio.herokuapp.com/']
+  config.action_cable.url = "wss://hidehide-portfolio.herokuapp.com/cable"
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
